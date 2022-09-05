12e2e443db835a411f8a94592e995ff564af4a89
kevin.fabian <kevin.fabian-henkcorp@farmaciasperuanas.pe>
2022-09-05 18:07:17 -0500

    API Gateway v1

---
+++ ms-admin-api-gateway/src/main/java/pe/edu/galaxy/training/java/ms/admin/api/gateway/MsAdminApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.galaxy.training.java.ms.admin.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAdminApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAdminApiGatewayApplication.class, args);
+	}
+
+}
