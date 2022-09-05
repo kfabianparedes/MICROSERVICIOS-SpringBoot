8411905f4210c947f81fa7b56d1057d597d438e6
kevin.fabian <kevin.fabian-henkcorp@farmaciasperuanas.pe>
2022-09-05 13:11:24 -0500

    Server Registry

--- ms.pedidos.gestion.productos/src/main/java/pe/edu/galaxy/training/java/ms/pedidos/gestion/productos/Application.java
+++ ms.pedidos.gestion.productos/src/main/java/pe/edu/galaxy/training/java/ms/pedidos/gestion/productos/Application.java
@@ -2,7 +2,9 @@ package pe.edu.galaxy.training.java.ms.pedidos.gestion.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
